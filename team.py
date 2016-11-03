6b531e8629f9278a8b62490069994480858a2ed6
Alexandro Mayoral <alexandro.mayoral@gmail.com>
2016-11-02 19:49:54 -0600

    Add files via upload
    Correct code.

--- team.py
+++ team.py
@@ -1,29 +1,12 @@
 #!/usr/bin/python
-
 """
-    As the guys know each other very well, it didn’t take much
-    time for them to determine, how good is every person in
-    every role. Now, they have a 3 × 3 table, where the ﬁrst
-    row is for Andrew, the second row is for Peter, and the
-    third one is for Paul. The columns correspond to the roles:
-    ﬁrst to a “coder”, second to a “mathematician”, third to a
-    “tester”. As an example, the number in the third column of
-    the second row shows how good is Peter as a “tester”.
-
-    How the guys want to distribute the roles in such a way
-    that the team performs in a most eﬃcient way. Of course,
-    every person can take exactly one role, and every role
-    should be occupied by exactly one person. The eﬃciency of
-    the assignment where Andrew performs with the eﬃciency of
-    A, Peter performs with the eﬃciency of B and Paul with C,
-    is equal to sqrt(A^2 + B^2 + C^2).
+    Optimize to sqrt(A^2 + B^2 + C^2) where A, B, C, are the rows.
 """
 import math
 
 def ls_del (ls, idxs):
     t_ls = ls[:]
-    for i in idxs:
-        t_ls.pop(i)
+    t_ls = [i for j, i in enumerate(t_ls) if j not in idxs]
     return t_ls
 
 
